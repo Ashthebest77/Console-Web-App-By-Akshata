aa2fb7943cccac647dd9ee92e857a0ec0421d6c5
Ashthebest77 <90729719+Ashthebest77@users.noreply.github.com>
2022-03-06 14:12:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+<script>
+var first_name="Ranbir";
+var last_name="Kapoor";
+concat();
+first_name.concat(last_name);
+var a=10;
+var b=5;
+console.log (a);
+console.log (b);
+var add = a + b ;
+console.log(add);
+console.log (first_name.concat);
+console.log (last_name.concat);  
+</script>
